dd925075a2db14db0a877178210bd75507708dc3
fraleone99 <francesco8.leone@mail.polimi.it>
2021-03-27 21:25:55 +0100

    Creation of DevCardsSpace with Basic production

--- src/main/java/it/polimi/ingsw/model/GameBoard.java
+++ src/main/java/it/polimi/ingsw/model/GameBoard.java
@@ -5,6 +5,16 @@ import java.util.ArrayList;
 public class GameBoard {
     private int playersNumber;
     private Market market;
-    private ArrayList<PlayerDashboard> playersDashnoard;
+    private ArrayList<PlayerDashboard> playersDashboards = new ArrayList<>();
     private DevelopmentCardGrid developmentCardGrid;
+
+    public GameBoard(int playersNumber, ArrayList<Player> players) {
+        this.playersNumber = playersNumber;
+        market = new Market();
+        developmentCardGrid = new DevelopmentCardGrid();
+
+        for(int i = 0; i<playersNumber; i++){
+            playersDashboards.add(new PlayerDashboard(players.get(i).getNickname()));
+        }
+    }
 }
