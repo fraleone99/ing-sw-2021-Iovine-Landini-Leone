7641cee3d3efce11953ae2a0f5011a254718f570
Neeko <nicola.landini1999@gmail.com>
2021-05-24 16:40:21 +0200

    Market balls png, red cross png and fixes

--- src/main/java/it/polimi/ingsw/exceptions/NotExistingSpaceException.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.exceptions;
-
-public class NotExistingSpaceException extends Exception {
-    public NotExistingSpaceException(){
-        super("This popeSpace doesn't exist!");
-    }
-}
--- src/main/java/it/polimi/ingsw/exceptions/ShelfNotEmptyException.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.exceptions;
-
-public class ShelfNotEmptyException extends Exception {
-    public ShelfNotEmptyException(){
-        super("The Shelf is not empty!");
-    }
-}
