9ad1be4a710cb33edd03625c232e365e9de990aa
Neeko <nicola.landini1999@gmail.com>
2021-07-01 19:08:46 +0200

    View JavaDoc

--- src/main/java/it/polimi/ingsw/client/view/View.java
+++ src/main/java/it/polimi/ingsw/client/view/View.java
@@ -16,39 +16,105 @@ import java.util.ArrayList;
 
 public interface View {
 
+    /**
+     * Sets the handler for single or multi player
+     * @param handler handler that has to be set
+     */
     void setHandler(Handler handler);
 
+    /**
+     * This method is used to set up the connection
+     */
     void setupConnection();
 
+    /**
+     * Gets the IP
+     * @return the IP
+     */
     String getIp();
 
+    /**
+     * Gets the port number
+     * @return the port
+     */
     int getPortNumber();
 
+    /**
+     * Allows player to choose the game
+     * @return player's choice
+     */
     int gameType();
 
+    /**
+     * Welcomes the player
+     * @param welcome welcome string
+     */
     void handShake(String welcome);
 
-    //Method for Players' input
+    /**
+     * This method asks the player number
+     * @param message server question
+     */
     void askPlayerNumber(String message);
 
+    /**
+     * This method asks the client his nickname
+     * @param message server question
+     */
     void askNickname(String message);
 
+    /**
+     * This method handles client notification messages
+     * @param message notification
+     */
     void readMessage(String message);
 
+    /**
+     * This method allows the player to choose his initial resource
+     * @param message server question
+     */
     void askResource(String message);
 
+    /**
+     * This method allows the player to discard two of the four initial leader cards
+     * @param IdLeaders four initial leaders IDs
+     */
     void askLeaderToDiscard(ArrayList<Integer> IdLeaders);
 
+    /**
+     * This method allows the player to choose his turn action
+     * @param message server question
+     */
     void askTurnType(String message);
 
+    /**
+     * This method allows the player to active a leader
+     * @param message instance of ActiveLeader
+     */
     void activeLeader(ActiveLeader message);
 
+    /**
+     * This method allows the player to discard a leader
+     * @param message instance of DiscardLeader
+     */
     void discardLeader(DiscardLeader message);
 
+    /**
+     * This method asks the player what he wants to see from the game board
+     * @param message server question
+     */
     void seeGameBoard(String message);
 
+    /**
+     * This method handles client request of watching his leader cards
+     * @param leaderCards leader cards IDs
+     */
     void seeLeaderCards(ArrayList<Integer> leaderCards);
 
+    /**
+     * This method handles client request of watching the market
+     * @param market is the market
+     */
     void seeMarket(Market market);
 
     void chooseLine(String message);
